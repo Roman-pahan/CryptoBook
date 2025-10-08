d03058bf6edbddb9928fcf0b3ab9ea5e28f8e152
RoKorMan <mgpsiodrop@gmail.com>
2025-10-08 17:11:22 +0700

    added log in button

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,8 +1,26 @@
 "use client";
-import { Menu, MenuItem } from "semantic-ui-react";
+import { Button, Menu, MenuItem } from "semantic-ui-react";
 import Link from "next/link";
+import { useState } from "react";
 
 const Header = () => {
+  const [currentAccount, setCurrentAccount] = useState();
+
+  const handleLogInClick = async () => {
+    const { ethereum } = window;
+    if (!ethereum) {
+      alert("Установите кошелек!");
+    }
+    try {
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <Menu style={{ marginTop: "20px" }}>
       <Link href="/">
@@ -16,6 +34,19 @@ const Header = () => {
       <Link href="/show">
         <MenuItem>Посмотреть контакт</MenuItem>
       </Link>
+      <MenuItem position="right">
+        {!currentAccount ? (
+          <Button primary onClick={handleLogInClick}>
+            Вход
+          </Button>
+        ) : (
+          <Link href="/user">
+            <Button primary onClick={handleLogInClick}>
+              {currentAccount}
+            </Button>
+          </Link>
+        )}
+      </MenuItem>
     </Menu>
   );
 };
